3c945039f3ad8991ea86f86acf9835fcd410d6b7
Kenneth Fisher <kenandcrys@live.com>
2023-08-24 02:23:18 -0500

    one get

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -3,9 +3,15 @@ const router = express.Router();
 const md = require('./cars-middleware');
 const Car = require('./cars-model');
 
-router.get('/', async (req, res, next)=> {
-  res.json('getting all cars')
-})
+router.get('/', async (req, res, next) => {
+  try {
+    const cars = await Car.getAll();
+    const sortedCars = cars.sort((a, b) => a.id - b.id);
+    res.status(200).json(sortedCars);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get('/:id', async (req, res, next)=> {
   res.json(`getting car with id ${req.params.id}`)
